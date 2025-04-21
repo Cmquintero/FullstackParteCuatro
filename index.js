abde16bb377d69f4e9df17df67b873dad092337f
cmquintero <cmquinterot@ufpso.edu.co>
2025-04-21 04:22:30 -0500

    Parte3 ejercicio 3.4

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
 const express = require("express");
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-let notes =[
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
+let notes = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
   },
   {
-    id:5,
-    name:"Mario Carlos",
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+  {
+    id: 5,
+    name: "Mario Carlos",
     number: "3151451241",
-  }
-]
-
-
+  },
+];
 
 app.get("/api/persons/", (request, response) => {
   response.send(notes);
@@ -42,14 +40,14 @@ const info = () => {
   return `
     <p>Phonebook has info for ${total} people</p>
     <p>${date}</p>
-  `
+  `;
 };
 app.get("/api/info", (request, response) => {
   response.send(info());
 });
 
 app.get("/api/persons/5", (request, response) => {
-  const note = notes.find(n => n.id === 5);
+  const note = notes.find((n) => n.id === 5);
   if (note) {
     response.json(note);
   } else {
@@ -57,7 +55,14 @@ app.get("/api/persons/5", (request, response) => {
   }
 });
 
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+  response.send(`The person with id: ${id} has been delete`);
+  response.status(204).end();
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-})
+});
