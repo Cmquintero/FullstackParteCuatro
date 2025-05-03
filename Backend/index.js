bdbbe51548520c17a598b295d281f1ecc97000ba
cmquintero <cmquinterot@ufpso.edu.co>
2025-05-03 00:19:19 -0500

    Parte3 ejercicio3.9

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-const morgan = require('morgan')
+const morgan = require("morgan");
 app.use(morgan("tiny"));
-morgan.token('request-body',(request)=>{
-  return request.method === 'POST' ? JSON.stringify(request.body) : '';
-})
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :request-body"));
+const cors = require("cors");
+app.use(cors());
+morgan.token("request-body", (request) => {
+  return request.method === "POST" ? JSON.stringify(request.body) : "";
+});
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :request-body"
+  )
+);
 
 let notes = [
   {
@@ -34,17 +40,22 @@ let notes = [
     name: "Mario Carlos",
     number: "3151451241",
   },
+  {
+    id: 6,
+    name: "Doky TeAmo",
+    number: "25/04/2025",
+  },
 ];
 
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
 
-app.use(requestLogger)
+app.use(requestLogger);
 
 app.get("/api/persons/", (request, response) => {
   response.send(notes);
@@ -72,18 +83,18 @@ app.get("/api/persons/5", (request, response) => {
 });
 app.get("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);//crei que funcionaba igual que el.filter pero en realidad en esta linea busco solo un id mas no creo un array nuevo eliminando el elemento como lo hago en(*)
+  const note = notes.find((note) => note.id === id); //crei que funcionaba igual que el.filter pero en realidad en esta linea busco solo un id mas no creo un array nuevo eliminando el elemento como lo hago en(*)
 
   if (note) {
     response.json(note);
   } else {
     response.status(404).end();
   }
-}); 
+});
 
 app.delete("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);//(*)
+  notes = notes.filter((note) => note.id !== id); //(*)
   response.send(`The person with id: ${id} has been delete`);
   response.status(204).end();
 });
@@ -100,34 +111,34 @@ app.post("/api/persons", (request, response) => {
       error: "name or number missing",
     });
   }
-  if (notes.some(note => note.name === body.name)) {
+  if (notes.some((note) => note.name === body.name)) {
     return response.status(400).json({
-      error: "The name has been exist in the list"
-    }); //metodo para el manejo de errores 
+      error: "The name has been exist in the list",
+    }); //metodo para el manejo de errores
   }
   const generateRandomId = () => {
-    let id
+    let id;
     do {
-      id = Math.round(Math.random() * 1000000)//redonde ya que intente usar el ceil pero falle sin pensar que debia usar era 999999 y use el floor pero me parecia poco util decir si hacia arriba o a abajo
-    } while (notes.some(n => n.id === id))
-    return id
-  }
+      id = Math.round(Math.random() * 1000000); //redonde ya que intente usar el ceil pero falle sin pensar que debia usar era 999999 y use el floor pero me parecia poco util decir si hacia arriba o a abajo
+    } while (notes.some((n) => n.id === id));
+    return id;
+  };
   const note = {
     id: generateRandomId(),
     name: body.name,
     number: body.number,
   };
-  
+
   notes = notes.concat(note);
 
   response.json(note);
 });
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
---
+++ Frontend/src/services/notes.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/api/persons'
+
+/*const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
+    }*/
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  const nonExisting = {
+    content: 'If you import this note, this note doesnt exist in the form',
+    important: true,
+    }
+  return request.then((response) => response.data.concat(nonExisting))
+}
+   
+const create = newObject => {
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+const remove = id => {
+  return axios.delete(`${baseUrl}/${id}`).then(response => response.data)
+}
+
+export default { getAll, create, update, remove }
+//el código que maneja la comunicación con el backend 
