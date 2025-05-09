b377d934cb5367e5a7e6523ee7e401ec25abedaf
cmquintero <cmquinterot@ufpso.edu.co>
2025-05-09 01:29:36 -0500

    Parte3 ejercicio3.12

---
+++ mongo.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+  `mongodb+srv://cmquinterot:${password}@cluster0.4pro2ho.mongodb.net/personApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+/*note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
+*/
+
+const Person = mongoose.model('Person', personSchema)
+if (process.argv.length === 3) {
+
+  Person.find({}).then(result => {
+    console.log("Persons:")
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+} else {
+
+  const person = new Person({ name, number })
+
+  person.save().then(() => {
+    console.log(`Added ${name} number ${number} to persons`)
+    mongoose.connection.close()
+  })
+}
