2b60a28428ef0ddcda8957cbdddf61b6e3b37c6e
cmquintero <cmquinterot@ufpso.edu.co>
2025-06-11 18:23:40 -0500

    Parte4 Ejercicio4.4

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,4 +1,4 @@
-const { test } = require('node:test')
+const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
@@ -7,4 +7,59 @@ test('dummy returns one', () => {
 
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const listWithOneBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        upvote: 5,
+        __v: 0
+      }
+    ]
+
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const blogs = [
+      {
+        _id: '5a422aa71b54a676234d17f34',
+        title: 'tomoloca',
+        author: 'Diazlok',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        upvote: 3,
+        __v: 0
+      },
+      {
+        _id: '5a422aa71b54a676234dds7f34',
+        title: 'onepice',
+        author: 'Martines',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        upvote: 7,
+        __v: 0
+      },
+      {
+        _id: '32422aa71b54a676234dds7f34',
+        title: 'blackling',
+        author: 'choros',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        upvote: 10,
+        __v: 0
+      }
+    ]
+
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, 20)
+  })
 })
